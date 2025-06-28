fb9877aa6cd7654787e03bc523a20bd59dda7566
Mas Byte <gitlab@masbyte.com>
2025-06-27 19:39:15 -0600

    feat(index):se extrae de un excel titulo, fecha y orden cronologico y se muestra en la peticion principal como json

--- MCUBack/index.js
+++ MCUBack/index.js
@@ -1,5 +1,19 @@
 import express from 'express';
 import fs from 'fs';
+import XlsxPopulate from 'xlsx-populate';
+
+async function peliculas (){
+    try {
+    const workbook = await XlsxPopulate.fromFileAsync('./marvel.xlsx')
+    const value = workbook.sheet('Hoja1').range('A1:C531').value()
+    //console.log(value).
+    return JSON.stringify (value);
+    }catch (error){
+        console.error(error);
+    }
+}
+
+//main();
 
 const PORT = 3000;
 const app = express();
@@ -13,8 +27,10 @@ const readDate = () => {
         return 'Unknown Date';
     }
 };
-app.get('/', (req, res) => {
-    res.send('Hello, Marvel Universe!');
+app.get('/', async (req, res) => {
+    console.log(peliculas());
+    const todo = await peliculas();
+    res.json(todo);
 });
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
